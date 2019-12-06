0d98a1896ae26393a55b78295ffb523ba199bee4
Vikram <vikramshk@gmail.com>
2019-12-06 15:33:36 +0800

    Database

---
+++ db.py
@@ -0,0 +1,20 @@
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
+
+# Create table
+try:
+	c.execute('''CREATE TABLE users
+		     (username text, password text, isAdmin integer)''')
+
+	# Insert a row of data
+	c.execute("INSERT INTO users VALUES ('a','{}',1)".format(make_pw_hash("myPassw0rd")))
+	c.execute("INSERT INTO users VALUES ('b','{}',0)".format(make_pw_hash("hello123")))
+except:
+	print("Table already created")
+
+adminList = ["a"]
+
+# Save (commit) the changes
+conn.commit()
+conn.close
+
